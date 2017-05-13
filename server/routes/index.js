aa963a942d49d741e77f2857e9c6aeced4a1973f
Thomas Bormans <thomas.bormans@district01.be>
2017-05-13 17:59:12 +0200

    Status route added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,19 @@
 require("rootpath")();
 const glob = require("glob");
+const version = require("package.json").version;
 
 module.exports = (app) => {
 	const routes = glob.sync("server/routes/!(index).js");
 
 	routes.forEach(route => {
 		require(route)(app);
+	});
+
+	app.route("/status").get((req, res) => {
+		res.status(200).json({
+			success: true,
+			version: version,
+		});
 	});
 
 	// Fallback route
