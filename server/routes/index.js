45c7be654e49f8dd35d08562c709b291a82842ab
Thomas Bormans <thomas.bormans@district01.be>
2017-05-15 09:00:33 +0200

    Docs added

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,44 @@ module.exports = (app) => {
 		require(route)(app);
 	});
 
+	/**
+	 * @swagger
+	 * /status:
+	 *   get:
+	 *     description: Status call of the server
+	 *     produces:
+	 *       - application/json
+	 *     parameters:
+	 *       - name: success
+	 *         description: Success boolean
+	 *         in: body
+	 *         required: true
+	 *         type: boolean
+	 *       - name: version
+	 *         description: App version
+	 *         in: body
+	 *         required: true
+	 *         type: string
+	 *     responses:
+	 *       200:
+	 *         description: OK
+	 */
 	app.route("/status").get(CoreController.status);
+
+	if (process.env.NODE_ENV === "local") { // Only allow docs in local environment
+		/**
+		 * @swagger
+		 * /docs:
+		 *   get:
+		 *     description: Documentation of all API's
+		 *     produces:
+		 *       - text/html
+		 *     responses:
+		 *       200:
+		 *         description: OK
+		 */
+		app.route("/docs").get(CoreController.docs);
+	}
 
 	// Fallback route
 	app.route(["/", "/*"]).all((req, res) => {
