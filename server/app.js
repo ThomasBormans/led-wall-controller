76c3220e55766f947d6cca24c5c6f959e582fa8a
Thomas Bormans <thomas.bormans@district01.be>
2017-05-16 21:00:04 +0200

    Handle errors

--- server/app.js
+++ server/app.js
@@ -31,6 +31,9 @@ require("./middleware")(app);
 // Init routes
 require("./routes/")(app);
 
+// Set fallback for errors
+app.use(require("./middleware/errorHandler"));
+
 // Init server
 app.listen(config.server.port, () => {
 	console.log(`Server listening at port ${config.server.port}, running in ${process.env.NODE_ENV} mode.`); // eslint-disable-line no-console
